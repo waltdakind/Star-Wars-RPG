9d7933655a3e6b58214190b4a8842235e76aae1d
Walter Mattingly <waltdakind@gmail.com>
2016-02-23 19:10:26 -0500

    styling mostly done need to work on jquery events

---
+++ assets/javascript/game.js
@@ -0,0 +1,68 @@
+// 
+//========================================
+
+var players = {name: "Obi-Wan Kenobi", healthPoints:100, attackPower:10 , counterAttackPower:50 ,"
+name: "Luke Skywalker", healthPoints: 120, attackPower:20 , counterAttackPower:35 
+name: "Darth Sidious", healthPoints: 150, attackPower:40 , counterAttackPower:12
+Name: "Darth Maul", healthPoints: 180, attackPower:40 , counterAttackPower:25}
+
+{"players":[
+    {"name":"Obi-Wan Kenobi", "image": , "hitPoints":120},
+    {"name":"Luke Skywalker", "hitPoints":100},
+    {"name":"Darth Sidious", "hitPoints":150},
+   {"name":"Darth Maul", "hitPoints":180}
+]}
+
+//Health Points
+//Attack Power (incremental increase)
+//Counter Attack Power (fixed)
+
+//FUNCTIONS
+//=====================================================================
+	//Picking a player
+	//=================
+	$( "img .box" ).click(function() {
+  alert( "The click registered!" );
+});
+	//Picking a target Enemy
+	//=================
+	function enemyChoice() {
+		console.log();
+	}
+
+	function playerAttack() {
+		console.log();
+	}
+
+	function enemyAttack() {
+		console.log();
+	}
+
+	function roundComplete() {
+		console.log();
+	}
+
+	function increaseAttackPower() {
+
+	}
+
+
+
+$(document).ready(function() {
+
+
+
+
+
+
+
+
+
+
+//MAIN PROCESS
+//=====================================================================
+
+
+
+
+	
