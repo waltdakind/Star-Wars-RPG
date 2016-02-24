f1606bda769de2ce571db3c6114508c2ca6d2103
Walter Mattingly <waltdakind@gmail.com>
2016-02-23 19:48:53 -0500

    styling mostly done need to work on jquery events

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,29 @@
-// 
+// class of objects -- players
 //========================================
 
-var players = {name: "Obi-Wan Kenobi", healthPoints:100, attackPower:10 , counterAttackPower:50 ,"
-name: "Luke Skywalker", healthPoints: 120, attackPower:20 , counterAttackPower:35 
-name: "Darth Sidious", healthPoints: 150, attackPower:40 , counterAttackPower:12
-Name: "Darth Maul", healthPoints: 180, attackPower:40 , counterAttackPower:25}
 
-{"players":[
-    {"name":"Obi-Wan Kenobi", "image": , "hitPoints":120},
-    {"name":"Luke Skywalker", "hitPoints":100},
-    {"name":"Darth Sidious", "hitPoints":150},
-   {"name":"Darth Maul", "hitPoints":180}
-]}
+var players = [
+    {"name":"Obi-Wan Kenobi", hitPoints:120, "counterAttack":15, "image":"obi-wan.jpg"},
+    {"name":"Luke Skywalker", "hitPoints":100, "counterAttack":5, "image":"luke-skywalker.jpg"},
+    {"name":"Darth Sidious", "hitPoints":150, "counterAttack":20, "image":"darth-sidious.png"},
+   {"name":"Darth Maul", "hitPoints":180, "counterAttack":25, "image":"darth-maul.jpg"}
+]
+
+
+// loop through to update DOM
+// place name of character at top in .characters#name
+// place image variable in "<img src="assets/images/" +$(this)
+//place hit points in .character#hitPoints
+
+ for(var i = 0; i < players.length; i++) {
+ 	//create initial display here
+
+ 	$
+ }
+
+
+
+
 
 //Health Points
 //Attack Power (incremental increase)
@@ -21,39 +33,46 @@ Name: "Darth Maul", healthPoints: 180, attackPower:40 , counterAttackPower:25}
 //=====================================================================
 	//Picking a player
 	//=================
-	$( "img .box" ).click(function() {
-  alert( "The click registered!" );
-});
+	function pickPlayer = $( ".characters img" ).click(function(playerChosen) {
+     console.log('registered click 1')
+    }
+
+    function playerChosen { 
+    	console.log("They chose" + this.players.name);
+    }
+
+
+
+
 	//Picking a target Enemy
 	//=================
 	function enemyChoice() {
 		console.log();
 	}
 
-	function playerAttack() {
-		console.log();
-	}
 
-	function enemyAttack() {
-		console.log();
-	}
+	// function playerAttack() {
+	// 	console.log();
 
-	function roundComplete() {
-		console.log();
-	}
+	// }
 
-	function increaseAttackPower() {
+	// function enemyAttack() {
+	// 	console.log();
 
-	}
+	// }
 
+	// function roundComplete() {
+	// 	console.log();
+	// }
 
+	// function increaseAttackPower() {
 
-$(document).ready(function() {
+	// }
 
+$( document ).ready(function() {
+  // Handler for .ready() called.
 
-
-
-
+});
 
 
 
@@ -62,7 +81,7 @@ $(document).ready(function() {
 //MAIN PROCESS
 //=====================================================================
 
-
+pickplayer();
 
 
 	
